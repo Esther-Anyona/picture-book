03905640ea7451d35ae310d2e2cbd83c31afdaa2
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-26 19:21:13 +0300

    Add files to hide secret key

--- picture_book/settings.py
+++ picture_book/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p33u7cld^t3u=_br1vb7$nh0o@)!aj7djw+g&puyh4#%g19-d8'
+SECRET_KEY = ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
